5963ae385db0450093e3aa261a0a2663a6f38393
Pankaj Deswal <pankaj.deswal@keus.in>
2022-03-22 13:53:05 +0530

    2nd attempt to do changes in dev branch

--- diff.java
+++ diff.java
@@ -1,6 +1,6 @@
 public class Diff{
 	public static void main(String[] args) {
 
-      System.out.println("done changes in dev branch");
+      System.out.println("secong attempt to do changes in dev branch");
    }
 }
