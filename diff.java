5200f34f264c17d4fd8e4e3ed8d65952044c2081
Pankaj Deswal <pankaj.deswal@keus.in>
2022-03-22 13:50:58 +0530

    merged 1st time

--- diff.java
+++ diff.java
@@ -1,6 +1,10 @@
 public class Diff{
 	public static void main(String[] args) {
 
+<<<<<<< HEAD
       System.out.println("We are inside the diff file");
+=======
+      System.out.println("done changes in dev branch");
+>>>>>>> dev
    }
 }
